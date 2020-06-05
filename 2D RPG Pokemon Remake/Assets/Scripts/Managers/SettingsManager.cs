a05ac1656ae4914e1ac9430f968b91352b37c1b7
Philip Yuen <philipyuen@cmail.carleton.ca>
2020-06-05 11:15:55 -0400

    Added function to settings menu

--- 2D RPG Pokemon Remake/Assets/Scripts/Managers/SettingsManager.cs
+++ 2D RPG Pokemon Remake/Assets/Scripts/Managers/SettingsManager.cs
@@ -1,15 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsManager : MonoBehaviour {
 
-    public void VideoSettings() {
+    public GameObject settingsMenuUI;
+    public GameObject videoSettingsUI;
+    public GameObject audioSettingsUI;
 
+    public AudioMixer audioMixer;
+
+    public Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+
+    private void Start() {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> resOptions = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++) {
+            string resOption = resolutions[i].width + " x " + resolutions[i].height;
+            resOptions.Add(resOption);
+
+            if (resolutions[i].width == Screen.width
+                && resolutions[i].height == Screen.height) {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(resOptions);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
 
-    public void AudioSettings() {
+    /*----- Video Settings -----*/
+    public void ToggleVideoSettings() {
+        settingsMenuUI.SetActive(!settingsMenuUI.activeSelf);
+        videoSettingsUI.SetActive(!videoSettingsUI.activeSelf);
+    }
 
+    public void SetFullscreen (bool isFullscreen) {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution (int resolutionIndex) {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetQuality(int qualityIndex) {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+
+    /*----- Audio Settings -----*/
+    public void ToggleAudioSettings() {
+        settingsMenuUI.SetActive(!settingsMenuUI.activeSelf);
+        audioSettingsUI.SetActive(!audioSettingsUI.activeSelf);
+    }
+
+    public void SetVolume(float volume) {
+        audioMixer.SetFloat("Volume", volume);
     }
 
 }
